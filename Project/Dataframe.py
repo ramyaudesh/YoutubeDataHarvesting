f24f6e158204c92a18ee7d5599c89b4899ee13ba
ramya <ramyaudesh@gmail.com>
2024-04-03 02:49:09 +0530

    updated alchemy imports and db connections
    dbutility file deleted

--- Project/Dataframe.py
+++ Project/Dataframe.py
@@ -1,9 +1,7 @@
 import pandas as pd
 import streamlit as st
-from DbUtility import*
 from sqlalchemy import *
-
-ChannelDataList = []
+from mysql import *
 
 def ChannelData(data):
     FullChannelDetails = data
@@ -14,18 +12,20 @@ def ChannelData(data):
         "CHANNELTYPE" : [FullChannelDetails["items"][0]["kind"]],
         "CHANNELVIEWS" : [FullChannelDetails["items"][0]["statistics"]["viewCount"]],
         "CHANNELDESCRIPTION" : [FullChannelDetails["items"][0]["snippet"]["description"]],
-        "CHANNELSTATUS" : [FullChannelDetails["items"][0]["snippet"]["title"]]
+        "CHANNELSTATUS" : [FullChannelDetails["items"][0]["snippet"]["title"]]       
         })
-  
     StoredChannelData = pd.DataFrame(requiredChannelDetails)
-    ChannelDataList.append(StoredChannelData)
-    #return ChannelDataList
-    st.write(StoredChannelData)
+    engine = create_engine('mysql://ramya:root123@localhost/youtubeharvesting')
+    StoredChannelData.to_sql('channel', con=engine, if_exists='append', index=False)    
     
-def UploadChannelData(StoredChannelData):
-       
-    my_db = create_connection("localhost","ramya","root123","youtubeharvesting")
-    StoredChannelData.to_sql('channel', con=my_db, if_exists='append', index=False)
+    return requiredChannelDetails
+
+    
+def UploadChannelData(requiredChannelDetails):
+       if(requiredChannelDetails!=null):
+        StoredChannelData = pd.DataFrame(requiredChannelDetails)
+        engine = create_engine('mysql://ramya:root123@localhost/youtubeharvesting')
+        StoredChannelData.to_sql('channel', con=engine, if_exists='append', index=False)    
     
     
 def VideoData(data):
