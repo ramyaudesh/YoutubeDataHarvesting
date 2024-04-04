65293d4692f4f2731dd9b388658981477dfb583c
ramya <ramyaudesh@gmail.com>
2024-04-04 11:54:07 +0530

    drop down code

--- Project/MyFirstApp.py
+++ Project/MyFirstApp.py
@@ -27,6 +27,17 @@ if selectionMenu == "Channels":
     
 
 else:
-    st.title("Welcome to the Dashboard !!") 
+    st.title("Welcome to the Dashboard !!")
+    st.selectbox(label= 'Choose the options', options=[
+            'What are the names of all the videos and their corresponding channels?',
+            'Which channels have the most number of videos, and how many videos do they have?',
+            'What are the top 10 most viewed videos and their respective channels?',
+            'How many comments were made on each video, and what are their corresponding video names?',
+            'Which videos have the highest number of likes, and what are their corresponding channel names?',
+            'What is the total number of likes and dislikes for each video, and what are their corresponding video names?',
+            'What is the total number of views for each channel, and what are their corresponding channel names?',
+            'What are the names of all the channels that have published videos in the yea 2022?',
+            'What is the average duration of all videos in each channel, and what are their corresponding channel names?',
+            'Which videos have the highest number of comments, and what are their corresponding channel names?'])
 
 
